2495b5048d1804984ef84e60c72c040b8142a7ec
Nathaniel W. <52637288+nw-github@users.noreply.github.com>
2024-05-26 12:34:31 -0400

    fix the app

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,40 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
+  import Image from "./lib/Image.svelte";
+  import { COUNTERS } from "./lib/counters";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  <table>
+    {#each COUNTERS as [brawler, counters]}
+      <tr>
+        <td class="container">
+          <Image {brawler} />
+          <div class="bottom-left">{brawler}</div>
+        </td>
+        <td class="counters">
+          {#each counters as counter}
+            <Image brawler={counter} />
+          {/each}
+        </td>
+      </tr>
+    {/each}
+  </table>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .counters {
+    display: flex;
+    padding-left: 100px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  .container {
+    position: relative;
+    text-align: center;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  .bottom-left {
+    position: absolute;
+    bottom: 8px;
+    left: 16px;
   }
 </style>
---
+++ src/lib/Image.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { IMAGES } from "./counters";
+
+  export let brawler: string;
+</script>
+
+{#if IMAGES[brawler]}
+  <img src={IMAGES[brawler]} alt={brawler} />
+{:else}
+  <h1>{brawler}</h1>
+{/if}
+
+<style>
+  h1,
+  img {
+    width: 200px;
+    height: 200px;
+    padding: 2px 5px;
+  }
+
+  h1 {
+    text-align: center;
+    display: inline;
+    width: auto;
+    height: auto;
+  }
+</style>
